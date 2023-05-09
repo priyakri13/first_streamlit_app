d49a1cedaa1fd1eb0ae52ae51941420a7b2fa375
priyakri13 <132900485+priyakri13@users.noreply.github.com>
2023-05-09 19:27:36 +0530

    Update streamlit_app.py
    done

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,9 +31,9 @@ streamlit.dataframe(fruits_to_show)
 
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+
 try:
-  streamlit.write('The user entered ', fruit_choice)
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information")
   else:
@@ -42,7 +42,7 @@ try:
       streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
   streamlit.error()
-streamlit.stop()
+  streamlit.stop()
 
 import snowflake.connector
 
